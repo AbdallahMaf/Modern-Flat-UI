69b82d989464064aa74ce7628c852e68b1087f98
Mohamed Abdallah Fall <abdallahfall2020@gmail.com>
2022-12-07 13:32:24 +0000

    FormProducts

---
+++ ExerciceTest/Forms/FormNotifications.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExerciceTest.Forms
+{
+    public partial class Form1Notifications : Form
+    {
+        public Form1Notifications()
+        {
+            InitializeComponent();
+        }
+    }
+}
